1574c3fd966bf222c864e38b6ee031bed1f24104
Sonkararvind <arvindsonkar685@gmail.com>
2025-07-18 23:27:55 +0530

    new changes

--- Ecom_App/views.py
+++ Ecom_App/views.py
@@ -1,17 +1,6 @@
-
-# from django.views.generic import TemplateView
-
-
-# class Home(TemplateView):
-#     template_name = 'home.html'
-# from django.views.generic import TemplateView
-
-# class Home(TemplateView):
-#     template_name = 'home.html'
-  
-
 from django.views.generic import TemplateView
 from .models import Product,All_Product
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -20,12 +9,6 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context['products'] = Product.objects.all()[:6] 
-        print('context') # Limit to 6 latest products
+        context['all_products'] = All_Product.objects.all()[:6]  # fix here
+
         return context
-
-    def get_content_data(self, **kwargs):
-        content = super().get_content_data(**kwargs)
-
-        content['All_products'] = All_Product.objects.all()[:6] 
-        print('content') # Limit to 6 latest products
-        return content
