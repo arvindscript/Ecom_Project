ecdcdf09f30fd679f8f22520c8f30e7780797850
arvindscript <arvindsonkar685@gmail.com>
2025-07-30 12:13:22 +0530

    authicate

--- Ecom_App/views.py
+++ Ecom_App/views.py
@@ -1,17 +1,3 @@
-# from django.views.generic import TemplateView
-# from .models import Product,All_Product,Latest_Product
-
-
-# class HomePageView(TemplateView):
-#     template_name = 'home.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         context['products'] = Product.objects.all()[:6] 
-#         context['all_products'] = All_Product.objects.all()[:6]  # fix here
-#         context['lates_products'] = Latest_Product.objects.all()[:6]   
-#         return context
 
 # views.py
 from django.views.generic import TemplateView
@@ -20,6 +6,7 @@ from django.shortcuts import render, redirect
 from .models import Product, All_Product, Latest_Product,Blog_Product
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -32,6 +19,8 @@ class HomePageView(TemplateView):
         context['blog_products'] = Blog_Product.objects.all()[:3]
         return context
     
+
+
 
 # class SignInView(TemplateView):
 #     template_name = 'signin.html'
@@ -46,16 +35,23 @@ class HomePageView(TemplateView):
 #         user = authenticate(request, username=email, password=password)
 #         if user is not None:
 #             login(request, user)
-#             return redirect('home')  # Make sure 'home' is a named URL in your urls.py
+#             return redirect('home')  # Ensure 'home' is defined in urls.py with name='home'
 #         else:
 #             messages.error(request, 'Invalid email or password')
-#             return render(request, self.create.html)
-from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
+#             return render(request, self.template_name)  # ✅ Correct template re-render
 
-class SignInView(TemplateView):
+# class CreateInView(TemplateView):
+#     template_name = 'create.html'
+
+
+
+# from django.contrib.auth import authenticate, login
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from django.views import View
+# from .forms import SignUpForm  
+
+class SignInView(View):
     template_name = 'signin.html'
 
     def get(self, request):
@@ -64,11 +60,24 @@ class SignInView(TemplateView):
     def post(self, request):
         email = request.POST.get('email')
         password = request.POST.get('password')
-
         user = authenticate(request, username=email, password=password)
-        if user is not None:
+        if user:
             login(request, user)
-            return redirect('home')  # Ensure 'home' is defined in urls.py with name='home'
-        else:
-            messages.error(request, 'Invalid email or password')
-            return render(request, self.template_name)  # ✅ Correct template re-render
+            return redirect('home')
+        messages.error(request, 'Invalid email or password')
+        return render(request, self.template_name)
+
+class CreateInView(View):  
+    template_name = 'create.html'
+
+    def get(self, request):
+        form = SignInView()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = SignInView(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
--- Ecom_Project/urls.py
+++ Ecom_Project/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from Ecom_App.views import HomePageView,SignInView
+from Ecom_App.views import HomePageView,SignInView,CreateInView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('signin/', SignInView.as_view(), name='signin'), 
-    #  path('signup/',SignInView.as_view(), name='signin'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('create/', CreateInView.as_view(), name='create'), 
+    #  path('signup/',SignView.as_view(), name='signin'),
 
 ]
 if settings.DEBUG:
